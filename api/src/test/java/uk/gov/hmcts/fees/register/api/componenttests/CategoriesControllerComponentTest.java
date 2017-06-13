034128d23b03c804b141674b6bbbd14451dca3f6
Sachi Kuppuswami <sachi>
2017-06-13 11:14:21 +0100

    Fix for hearing fees - claim amount above 3000 pounds

--- api/src/test/java/uk/gov/hmcts/fees/register/api/componenttests/CategoriesControllerComponentTest.java
+++ api/src/test/java/uk/gov/hmcts/fees/register/api/componenttests/CategoriesControllerComponentTest.java
@@ -29,19 +29,59 @@ public class CategoriesControllerComponentTest extends ComponentTestBase {
     }
 
     @Test
-    public void invalidRangeShouldResultIn404() throws Exception {
+    public void invalidRangeOnlineFeesShouldResultIn404() throws Exception {
         restActions
                 .get("/fees-register/cmc/categories/onlinefees/ranges/-1/fees")
                 .andExpect(status().isNotFound());
     }
+    @Test
+    public void invalidRangeOnlineFeesClaimAmountZeroShouldResultIn404() throws Exception {
+        restActions
+            .get("/fees-register/cmc/categories/onlinefees/ranges/0/fees")
+            .andExpect(status().isNotFound());
+    }
+
+
+
 
     @Test
-    public void validRangeShouldResultIn200() throws Exception {
+    public void validRangeOnlineFeesShouldResultIn200() throws Exception {
         restActions
                 .get("/fees-register/cmc/categories/onlinefees/ranges/1/fees")
                 .andExpect(status().isOk())
                 .andExpect(body().isEqualTo(new FixedFee("X0024", "Civil Court fees - Money Claims Online - Claim Amount - 0 upto 300", 2500)));
     }
+
+    @Test
+    public void invalidRangeHearingFeesClaimAmountZeroShouldResultIn404() throws Exception {
+        restActions
+            .get("/fees-register/cmc/categories/hearingfees/ranges/0/fees")
+            .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    public void validRangeHearingFeesForClaimAmount3000PoundsShouldResultIn200() throws Exception {
+        restActions
+            .get("/fees-register/cmc/categories/hearingfees/ranges/300000/fees")
+            .andExpect(status().isOk())
+            .andExpect(body().isEqualTo(new FixedFee("X0052", "Civil Court fees - Hearing fees - Claim Amount - 1500.01 upto 3000", 17000)));
+    }
+    @Test
+    public void validRangeHearingFeesForClaimAmountAbove3000PoundsShouldResultIn200() throws Exception {
+        restActions
+            .get("/fees-register/cmc/categories/hearingfees/ranges/300001/fees")
+            .andExpect(status().isOk())
+            .andExpect(body().isEqualTo(new FixedFee("X0053", "Civil Court fees - Hearing fees - Claim Amount from 3000.01", 33500)));
+    }
+
+    @Test
+    public void invalidRangeHearingFeesShouldResultIn404() throws Exception {
+        restActions
+            .get("/fees-register/cmc/categories/hearingfees/ranges/-1/fees")
+            .andExpect(status().isNotFound());
+    }
+
 
     @Test
     public void validFlatShouldResultIn200() throws Exception {
