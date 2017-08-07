08a250e6251724415b38ea10a46bb698e3ed65bb
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-08-07 14:06:04 +0100

    Test anonymous retrieval

--- api/src/test/java/uk/gov/hmcts/fees/register/api/componenttests/SecurityComponentTest.java
+++ api/src/test/java/uk/gov/hmcts/fees/register/api/componenttests/SecurityComponentTest.java
@@ -7,6 +7,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SecurityComponentTest extends ComponentTestBase {
 
     @Test
+    public void anonymousRetrieveFeeAllowed() throws Exception {
+        restActions.get("/fees").andExpect(status().isOk());
+        restActions.get("/fees/X0433").andExpect(status().isOk());
+    }
+
+    @Test
+    public void anonymousRetrieveCategoryAllowed() throws Exception {
+        restActions.get("/categories").andExpect(status().isOk());
+        restActions.get("/categories/cmc-online").andExpect(status().isOk());
+    }
+
+    @Test
+    public void anonymousRetrieveRangeGroupAllowed() throws Exception {
+        restActions.get("/range-groups").andExpect(status().isOk());
+        restActions.get("/range-groups/cmc-online").andExpect(status().isOk());
+    }
+
+    @Test
     public void anonymousUpdateFeeForbidden() throws Exception {
         restActions
             .put("/fees/X0433", "any body")
