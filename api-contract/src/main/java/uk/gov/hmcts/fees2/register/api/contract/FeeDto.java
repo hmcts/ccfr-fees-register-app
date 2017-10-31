7b285bdabe7d81d7bf654067955fa7cbfd954371
tpalisetty <tarun.palisetty@hmcts.net>
2017-10-31 08:51:53 +0000

    FeeDto added

---
+++ api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/FeeDto.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.fees2.register.api.contract;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ */
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Builder(builderMethodName = "feeDtoWith")
+public class FeeDto {
+
+    private String code;
+
+    private String memoLine;
+
+    private AmountTypeDto amountTypeDto;
+
+    private ChannelTypeDto channelTypeDto;
+
+    private DirectionTypeDto directionTypeDto;
+
+    private EventTypeDto eventTypeDto;
+
+    private FeeTypeDto feeTypeDto;
+
+    private Jurisdiction1Dto jurisdiction1Dto;
+
+    private Jurisdiction2Dto jurisdiction2Dto;
+
+    private ServiceTypeDto serviceTypeDto;
+
+}
