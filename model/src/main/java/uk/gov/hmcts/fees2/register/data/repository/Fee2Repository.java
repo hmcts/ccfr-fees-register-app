e0bc6010c898485bdb76e0f84e5ca263020306fe
tpalisetty <tarun.palisetty@hmcts.net>
2017-10-31 08:56:00 +0000

    Fee2Repository added

---
+++ model/src/main/java/uk/gov/hmcts/fees2/register/data/repository/Fee2Repository.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.fees2.register.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.fees2.register.data.exceptions.FeeNotFoundException;
+import uk.gov.hmcts.fees2.register.data.model.Fee;
+
+import java.util.Optional;
+
+/**
+ * Created by tarun on 30/10/2017.
+ */
+
+@Repository
+public interface Fee2Repository extends JpaRepository<Fee, Long> {
+
+    Optional<Fee> findByCode(String code);
+
+    default Fee findByCodeOrThrow(String code) {
+        return findByCode(code).orElseThrow(() -> new FeeNotFoundException(code));
+    }
+}
