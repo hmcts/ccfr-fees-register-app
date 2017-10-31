5dd11a7ac7e77e38ce0c33e97c24c81e509eb6a1
tpalisetty <tarun.palisetty@hmcts.net>
2017-10-31 08:53:18 +0000

    FeeVersion model added

---
+++ model/src/main/java/uk/gov/hmcts/fees2/register/data/model/FeeVersion.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.fees2.register.data.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by tarun on 30/10/2017.
+ */
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "feeVersionWith")
+@Table(name = "fee_version")
+public class FeeVersion extends AbstractEntity{
+
+    @ManyToOne
+    @JoinColumn(name = "fee_id")
+    @JsonManagedReference
+    private Fee fee;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "version")
+    private int version;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "valid_from")
+    private Date validFrom;
+
+    @Column(name = "valid_to")
+    private Date validTo;
+
+    @Column(name = "min_range")
+    private Long minRange;
+
+    @Column(name = "max_range")
+    private Long maxRange;
+
+    @Column(name = "fee_amount")
+    private Long feeAmount;
+
+    @Column(name = "min_fee_amount")
+    private Long minFeeAmount;
+
+    @Column(name = "max_fee_amount")
+    private Long maxFeeAmount;
+
+    @Column(name = "percentage")
+    private BigDecimal percentage;
+
+}
