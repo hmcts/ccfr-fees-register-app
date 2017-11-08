af30a3b4ad052937a22ba39840cbf6cb93ba6e70
tpalisetty <tarun.palisetty@hmcts.net>
2017-11-08 10:38:28 +0000

    Table annotation added

--- model/src/main/java/uk/gov/hmcts/fees2/register/data/model/FixedFee.java
+++ model/src/main/java/uk/gov/hmcts/fees2/register/data/model/FixedFee.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.fees2.register.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "fixed_fee")
 public class FixedFee extends Fee{
 
     @Override
