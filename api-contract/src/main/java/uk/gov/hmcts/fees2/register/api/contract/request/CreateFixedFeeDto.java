528b0bc26dbdf1bcf89b5ff72b921f43e426cf51
tpalisetty <tarun.palisetty@hmcts.net>
2017-11-09 16:06:23 +0000

    Api contract modified

--- api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/request/CreateFixedFeeDto.java
+++ api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/request/CreateFixedFeeDto.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.fees2.register.api.contract.FeeVersionDto;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 public class CreateFixedFeeDto extends CreateFeeDto{
 
+    public CreateFixedFeeDto(String code, FeeVersionDto version, String jurisdiction1, String jurisdiction2, String service, String channel, String direction, String event, String memoLine, String feeOrderName, String naturalAccountCode) {
+        super(code, version, jurisdiction1, jurisdiction2, service, channel, direction, event, memoLine, feeOrderName, naturalAccountCode);
+    }
 }
--- api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/request/CreateRangedFeeDto.java
+++ api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/request/CreateRangedFeeDto.java
@@ -13,10 +13,14 @@ import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class CreateRangedFeeDto extends CreateFeeDto{
+
+    public CreateRangedFeeDto(String code, FeeVersionDto version, String jurisdiction1, String jurisdiction2, String service, String channel, String direction, String event, String memoLine, String feeOrderName, String naturalAccountCode, BigDecimal maxRange) {
+        super(code, version, jurisdiction1, jurisdiction2, service, channel, direction, event, memoLine, feeOrderName, naturalAccountCode);
+        this.maxRange = maxRange;
+        this.minRange = minRange;
+    }
 
     private BigDecimal minRange;
 
