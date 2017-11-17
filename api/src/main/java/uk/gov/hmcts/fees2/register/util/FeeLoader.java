05273b7363d4ce8ea31d74fe8c908bd9827358c3
tpalisetty <tarun.palisetty@hmcts.net>
2017-11-17 12:53:45 +0000

    PAY-474 Fee loader changes to support Fixed and Ranged fee loading

--- api/src/main/java/uk/gov/hmcts/fees2/register/util/FeeLoader.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/util/FeeLoader.java
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.fees2.register.api.contract.request.CreateFeeDto;
+import uk.gov.hmcts.fees2.register.api.contract.request.CreateFixedFeeDto;
 import uk.gov.hmcts.fees2.register.api.contract.request.CreateRangedFeeDto;
 import uk.gov.hmcts.fees2.register.api.controllers.FeeController;
 import uk.gov.hmcts.fees2.register.api.controllers.mapper.FeeDtoMapper;
@@ -45,8 +47,14 @@ public class FeeLoader implements ApplicationRunner {
 
             feeDtos.forEach(fee -> {
                try {
-                   feeController.createRangedFee(fee, null);
-                   LOG.info("Fee with code " + fee.getCode() + " inserted into database.");
+                   if (fee.getUnspecifiedClaimAmount() != null && fee.getUnspecifiedClaimAmount().equals(true)) {
+                       CreateFixedFeeDto fixedFee = populateFixedFeeDto(fee);
+                       feeController.createFixedFee(fixedFee, null);
+                       LOG.info("Fixed fee with code " +fee.getCode()+ " loaded into database.");
+                   } else {
+                       feeController.createRangedFee(fee, null);
+                       LOG.info("Ranged fee with code " +fee.getCode()+ " loaded into database.");
+                   }
                } catch (BadRequestException be) {
                    LOG.info("Fee code " + fee.getCode() + " already in use");
                }
@@ -55,6 +63,7 @@ public class FeeLoader implements ApplicationRunner {
 
         } catch (IOException  | NullPointerException ex) {
             LOG.error("Error is loading cmc fee json loader");
+            throw new IOException("Error in loading fee into the database.");
         }
 
 
@@ -63,5 +72,20 @@ public class FeeLoader implements ApplicationRunner {
     private List<CreateRangedFeeDto> loadFromResource(String location) throws IOException {
         InputStream fileAsInputStream = resourceLoader.getResource(location).getInputStream();
         return objectMapper.readValue(fileAsInputStream, objectMapper.getTypeFactory().constructCollectionType(List.class, CreateRangedFeeDto.class));
+    }
+
+    private CreateFixedFeeDto populateFixedFeeDto(CreateRangedFeeDto rangedFeeDto) {
+        CreateFixedFeeDto fixedFee = new CreateFixedFeeDto();
+        fixedFee.setCode(rangedFeeDto.getCode());
+        fixedFee.setChannel(rangedFeeDto.getChannel());
+        fixedFee.setDirection(rangedFeeDto.getDirection());
+        fixedFee.setEvent(rangedFeeDto.getEvent());
+        fixedFee.setJurisdiction1(rangedFeeDto.getJurisdiction1());
+        fixedFee.setJurisdiction2(rangedFeeDto.getJurisdiction2());
+        fixedFee.setService(rangedFeeDto.getService());
+        fixedFee.setUnspecifiedClaimAmount(rangedFeeDto.getUnspecifiedClaimAmount());
+        fixedFee.setVersion(rangedFeeDto.getVersion());
+
+        return fixedFee;
     }
 }
