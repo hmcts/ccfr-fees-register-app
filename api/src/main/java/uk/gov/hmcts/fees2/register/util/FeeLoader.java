17286d65dbff7eacef5d48e16ebd0efa75b71a6f
tpalisetty <tarun.palisetty@hmcts.net>
2017-11-20 12:00:44 +0000

    PAY-474 Fee loader refactored to support Fixed and Ranged fee json data

--- api/src/main/java/uk/gov/hmcts/fees2/register/util/FeeLoader.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/util/FeeLoader.java
@@ -9,12 +9,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.fees2.register.api.contract.request.CreateFeeDto;
 import uk.gov.hmcts.fees2.register.api.contract.request.CreateFixedFeeDto;
 import uk.gov.hmcts.fees2.register.api.contract.request.CreateRangedFeeDto;
 import uk.gov.hmcts.fees2.register.api.controllers.FeeController;
-import uk.gov.hmcts.fees2.register.api.controllers.mapper.FeeDtoMapper;
-import uk.gov.hmcts.fees2.register.data.exceptions.BadRequestException;
+import uk.gov.hmcts.fees2.register.api.controllers.mapper.FeeLoaderJsonMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,8 +26,8 @@ import java.util.List;
 public class FeeLoader implements ApplicationRunner {
     private static final Logger LOG = LoggerFactory.getLogger(FeeLoader.class);
 
-    @Value("classpath:${ranged.fees.loader}")
-    private String rangedFeesJsonInputFile;
+    @Value("classpath:${fees.loader.json}")
+    private String feesJsonInputFile;
 
     @Autowired
     private ResourceLoader resourceLoader;
@@ -43,23 +41,24 @@ public class FeeLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try {
-            List<CreateRangedFeeDto> feeDtos = loadFromResource(rangedFeesJsonInputFile);
+            FeeLoaderJsonMapper feeLoaderMapper = loadFromResource(feesJsonInputFile);
 
-            feeDtos.forEach(fee -> {
-               try {
-                   if (fee.getUnspecifiedClaimAmount() != null && fee.getUnspecifiedClaimAmount().equals(true)) {
-                       CreateFixedFeeDto fixedFee = populateFixedFeeDto(fee);
-                       feeController.createFixedFee(fixedFee, null);
-                       LOG.info("Fixed fee with code " +fee.getCode()+ " loaded into database.");
-                   } else {
-                       feeController.createRangedFee(fee, null);
-                       LOG.info("Ranged fee with code " +fee.getCode()+ " loaded into database.");
-                   }
-               } catch (BadRequestException be) {
-                   LOG.info("Fee code " + fee.getCode() + " already in use");
-               }
+            if (feeLoaderMapper.getFixedFees().size() > 0) {
+                List<CreateFixedFeeDto> fixedFees = feeLoaderMapper.getFixedFees();
+                fixedFees.forEach(f -> {
+                    feeController.createFixedFee(f, null);
+                    LOG.info("Fixed fee with code " +f.getCode()+ " inserted into database.");
+                });
+            }
 
-            });
+            if (feeLoaderMapper.getRangedFees().size() > 0) {
+                List<CreateRangedFeeDto> rangedFees = feeLoaderMapper.getRangedFees();
+                rangedFees.forEach(r -> {
+                    feeController.createRangedFee(r, null);
+                    LOG.info("Ranged fee with code " +r.getCode()+ " inserted into database.");
+                });
+            }
+
 
         } catch (IOException  | NullPointerException ex) {
             LOG.error("Error is loading cmc fee json loader");
@@ -69,23 +68,9 @@ public class FeeLoader implements ApplicationRunner {
 
     }
 
-    private List<CreateRangedFeeDto> loadFromResource(String location) throws IOException {
+    private FeeLoaderJsonMapper loadFromResource(String location) throws IOException {
         InputStream fileAsInputStream = resourceLoader.getResource(location).getInputStream();
-        return objectMapper.readValue(fileAsInputStream, objectMapper.getTypeFactory().constructCollectionType(List.class, CreateRangedFeeDto.class));
+        return objectMapper.readValue(fileAsInputStream, FeeLoaderJsonMapper.class);
     }
 
-    private CreateFixedFeeDto populateFixedFeeDto(CreateRangedFeeDto rangedFeeDto) {
-        CreateFixedFeeDto fixedFee = new CreateFixedFeeDto();
-        fixedFee.setCode(rangedFeeDto.getCode());
-        fixedFee.setChannel(rangedFeeDto.getChannel());
-        fixedFee.setDirection(rangedFeeDto.getDirection());
-        fixedFee.setEvent(rangedFeeDto.getEvent());
-        fixedFee.setJurisdiction1(rangedFeeDto.getJurisdiction1());
-        fixedFee.setJurisdiction2(rangedFeeDto.getJurisdiction2());
-        fixedFee.setService(rangedFeeDto.getService());
-        fixedFee.setUnspecifiedClaimAmount(rangedFeeDto.getUnspecifiedClaimAmount());
-        fixedFee.setVersion(rangedFeeDto.getVersion());
-
-        return fixedFee;
-    }
 }
