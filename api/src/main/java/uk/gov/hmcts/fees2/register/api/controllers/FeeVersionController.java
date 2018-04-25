36c40fa6233623a652c6904e71172571418c3353
Sateesh Mathangi <a-smathangi@hotels.com>
2018-04-25 15:32:39 +0100

    added separate end points for approve, reject and submit-for-review

--- api/src/main/java/uk/gov/hmcts/fees2/register/api/controllers/FeeVersionController.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/api/controllers/FeeVersionController.java
@@ -9,13 +9,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.fees2.register.api.contract.FeeVersionDto;
 import uk.gov.hmcts.fees2.register.api.controllers.mapper.FeeDtoMapper;
 import uk.gov.hmcts.fees2.register.data.model.FeeVersionStatus;
 import uk.gov.hmcts.fees2.register.data.service.FeeVersionService;
 
 import java.security.Principal;
+
+import static java.util.Optional.ofNullable;
+import static uk.gov.hmcts.fees2.register.data.model.FeeVersionStatus.approved;
+import static uk.gov.hmcts.fees2.register.data.model.FeeVersionStatus.pending_approval;
+import static uk.gov.hmcts.fees2.register.data.model.FeeVersionStatus.rejected;
 
 @Api(value = "FeesRegister", description = "Operations pertaining to fees")
 @RestController
@@ -70,6 +81,34 @@ public class FeeVersionController {
         Principal principal) {
 
         feeVersionService.changeStatus(feeCode, version, status, principal.getName());
+    }
+
+    @PatchMapping("/fees/{feeCode}/versions/{version}/approve")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void approve(
+        @PathVariable("feeCode") String feeCode,
+        @PathVariable("version") Integer version,
+        Principal principal) {
+
+        feeVersionService.changeStatus(feeCode, version, approved, ofNullable(principal)
+            .map(p -> p.getName())
+            .orElse(null));
+    }
+
+    @PatchMapping("/fees/{feeCode}/versions/{version}/reject")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void reject(
+        @PathVariable("feeCode") String feeCode,
+        @PathVariable("version") Integer version,
+        Principal principal) {
+
+        feeVersionService.changeStatus(feeCode, version, rejected, null);
+    }
+
+    @PatchMapping("/fees/{feeCode}/versions/{version}/submit-for-review")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void submitForReview(@PathVariable("feeCode") String feeCode, @PathVariable("version") Integer version) {
+        feeVersionService.changeStatus(feeCode, version, pending_approval, null);
     }
 
 }
