76f37aefff070f64fb2347b18667c8d5907dc91c
mswirski <mateusz.swirski@hmcts.net>
2018-06-07 10:30:50 +0100

    refactored and ignored some tests

--- api/src/functionalTest/java/uk/gov/hmcts/fees/register/functional/BuildInfoIntegrationTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/fees/register/functional/BuildInfoIntegrationTest.java
@@ -2,13 +2,14 @@ package uk.gov.hmcts.fees.register.functional;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.fees.register.functional.dsl.FeesRegisterTestDsl;
 
 import java.io.IOException;
 
-
+@Ignore
 public class BuildInfoIntegrationTest extends IntegrationTestBase{
 
     @Autowired
@@ -16,11 +17,11 @@ public class BuildInfoIntegrationTest extends IntegrationTestBase{
 
     @Test
     public void buildInfoShouldBePresent() throws IOException, NoSuchFieldException {
-//        scenario.given()
-//                .when().getBuildInfo()
-//                .then().got(JsonNode.class, response -> {
-//                    Assertions.assertThat(response.at("/git/commit/id").asText()).isNotEmpty();
-//                    Assertions.assertThat(response.at("/build/version").asText()).isNotEmpty();
-//                });
+        scenario.given()
+                .when().getBuildInfo()
+                .then().got(JsonNode.class, response -> {
+                    Assertions.assertThat(response.at("/git/commit/id").asText()).isNotEmpty();
+                    Assertions.assertThat(response.at("/build/version").asText()).isNotEmpty();
+                });
     }
 }
