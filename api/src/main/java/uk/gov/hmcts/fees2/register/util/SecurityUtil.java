e0d44456219e97cadc8374dea0b5c56d06c4dbf7
Sateesh Mathangi <Sateesh.Mathangi@kcom.com>
2018-09-17 12:48:19 +0100

    removed unused log

--- api/src/main/java/uk/gov/hmcts/fees2/register/util/SecurityUtil.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/util/SecurityUtil.java
@@ -1,13 +1,9 @@
 package uk.gov.hmcts.fees2.register.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtil {
-    private static final Logger LOG = LoggerFactory.getLogger(SecurityUtil.class);
-
     public static boolean hasRole(String role) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getAuthorities().stream()
