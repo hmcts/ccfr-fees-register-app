47b9a0dae7d44add9ceb2b88afd51d9e9798ea74
Sateesh Mathangi <a-smathangi@hotels.com>
2018-09-19 15:49:40 +0100

    PAY-1324 - added vault config

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,11 @@
+output "vaultUri" {
+  value = "${data.azurerm_key_vault.fees_key_vault.vault_uri}"
+}
+
+output "vaultName" {
+  value = "${local.vaultName}"
+}
+
 output "idam_api_url" {
   value = "${var.idam_api_url}"
 }
