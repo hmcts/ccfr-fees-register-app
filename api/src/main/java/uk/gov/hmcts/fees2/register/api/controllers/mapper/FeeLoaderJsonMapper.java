6b3e8c06c9bfe8793fb17e793682ec4975a022d2
mswirski <mateusz.swirski@hmcts.net>
2019-01-15 12:05:42 +0000

    fixed functional tests

--- api/src/main/java/uk/gov/hmcts/fees2/register/api/controllers/mapper/FeeLoaderJsonMapper.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/api/controllers/mapper/FeeLoaderJsonMapper.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.fees2.register.api.controllers.mapper;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.fees2.register.api.contract.loader.request.LoaderFixedFeeDto;
@@ -17,12 +19,13 @@ import java.util.List;
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Builder(builderMethodName = "feeLoaderJsonMapperWith")
+@AllArgsConstructor
+@NoArgsConstructor
 public class FeeLoaderJsonMapper {
 
     private static final Logger LOG = LoggerFactory.getLogger(FeeLoaderJsonMapper.class);
 
     private List<LoaderRangedFeeDto> rangedFees;
     private List<LoaderFixedFeeDto> fixedFees;
-
 
 }
