ceec894f5b983bd07d55330b6117970f3f798ff0
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2019-11-05 16:47:33 +0000

    PAY-2497:Aks related changes

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -13,8 +13,3 @@ output "OAUTH2_CLIENT_ID" {
 output "OAUTH2_REDIRECT_URI" {
   value = "${var.frontend_url}/oauth2"
 }
-
-# this variable will be accessible to tests as API_GATEWAY_URL environment variable
-output "api_gateway_url" {
-  value = "https://core-api-mgmt-${var.env}.azure-api.net/${local.api_base_path}"
-}
