aeb785db5405b09279097f7910f838ba4180f5c1
463952 <subroto.mukherjee@HMCTS.NET>
2020-05-17 16:41:09 +0100

    PAY-2874

--- api/src/main/java/uk/gov/hmcts/fees2/register/util/SecurityUtils.java
+++ api/src/main/java/uk/gov/hmcts/fees2/register/util/SecurityUtils.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.fees.register.api.filter.UserAuthVerificationFilter;
 import uk.gov.hmcts.fees.register.api.repositories.IdamRepository;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
