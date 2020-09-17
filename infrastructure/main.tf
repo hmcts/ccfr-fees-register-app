23e574808ce703cab48a295f5976a442b59970e1
Anil Dhawale <anil.dhawale@hmcts.net>
2020-09-17 13:37:27 +0100

    DB Upgrade

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -77,7 +77,8 @@ module "fees-register-database" {
   sku_tier = "GeneralPurpose"
   common_tags     = "${var.common_tags}"
   subscription = "${var.subscription}"
-}
+  postgresql_version = "${var.postgresql_version}"
+  }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "${var.component}-POSTGRES-PASS"
