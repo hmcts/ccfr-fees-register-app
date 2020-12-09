98e498e4cc18335cc6f9934c3ff2284e92675c16
Tim Jacomb <tim.jacomb@hmcts.net>
2020-12-09 16:50:39 +0000

    DTSPO-266 Enable self service read only access

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -68,7 +68,9 @@ resource "azurerm_key_vault_secret" "freg-idam-client-secret" {
 
 module "fees-register-database" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = join("-", [var.product, "postgres-db"])
+  product = var.product
+  component = var.component
+  name = join("-", [var.product, "postgres-db"])
   location = var.location
   env = var.env
   postgresql_user = var.postgresql_user
@@ -82,7 +84,9 @@ module "fees-register-database" {
 
 module "fees-register-database-v11" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = join("-", [var.product, "postgres-db-v11"])
+  product = var.product
+  component = var.component
+  name = join("-", [var.product, "postgres-db-v11"])
   location = var.location
   env = var.env
   postgresql_user = var.postgresql_user
