af0b88bce3d8e5d5194ed8e61d88a7c3389ebcb2
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-05-10 15:39:49 +0100

    updated

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,17 +10,17 @@ variable "env" {}
 
 variable "subscription" {}
 
-variable "ilbIp"{}
+variable "ilbIp" {}
 
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
+  default     = ""
 }
 
 variable "database_name" {
@@ -33,6 +33,17 @@ variable "postgresql_user" {
 
 variable "common_tags" {
   type = map(string)
+}
+
+variable "product_name" {
+  type    = string
+  default = "feeregister"
+
+}
+
+variable "feeregister_api_gateway_certificate_thumbprints" {
+  type    = list(any)
+  default = [] # TODO: remove default and provide environment-specific values
 }
 
 variable "core_product" {
