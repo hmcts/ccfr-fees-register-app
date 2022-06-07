0f772d60b6a64db5fdf401b01455c3ab285fd2a5
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-06-07 16:49:30 +0100

    Update fee_reg.tf

--- infrastructure/fee_reg.tf
+++ infrastructure/fee_reg.tf
@@ -19,7 +19,7 @@ module "ccpay-feeregister-api" {
   service_url   = local.feeregister_api_url
   product_id    = module.ccpay-feeregister-product.product_id
   name          = join("-", [var.product_name, "apiList"])
-  protocols     = ["https"]
+  protocols     = ["https" , "http"]
   display_name  = "Fee Register API"
   path          = "feeRegister-api"
   swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.freg_api.json"
