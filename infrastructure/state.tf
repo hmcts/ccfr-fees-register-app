4f368b3f77ee9b650961d18635a7b858e741df57
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-06-21 09:56:54 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -16,5 +16,5 @@ terraform {
 provider "azurerm" {
   alias = "cftappsdemo"
   features {}
-  subscription_id = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+  subscription_id = var.aks_subscription_id
 }
