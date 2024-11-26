f285546202aa0ab4af6d71b99802fc58ce3f23fb
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-11-26 12:00:06 +0000

    PAY-7209 remove JsonFormat

--- api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/FeeVersionDto.java
+++ api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/FeeVersionDto.java
@@ -28,11 +28,9 @@ public class FeeVersionDto {
     private Integer version;
 
     @JsonProperty("valid_from")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "GMT")
     private Date validFrom;
 
     @JsonProperty("valid_to")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "GMT")
     private Date validTo;
 
     private String description;
