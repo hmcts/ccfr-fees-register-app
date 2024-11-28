63fb1b78cbfcf0d54d3c7f402869083fc3dc8d68
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2024-11-28 15:13:24 +0000

    PAY-7209 remove JsonFormat

--- api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/amount/PercentageAmountDto.java
+++ api-contract/src/main/java/uk/gov/hmcts/fees2/register/api/contract/amount/PercentageAmountDto.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.fees2.register.api.contract.amount;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +11,6 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class PercentageAmountDto {
 
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
     private BigDecimal percentage;
 
 }
