1185595a5d3159dbe44a5c690b7fba4913b3be0e
Ollie-Smith <120727833+Ollie-Smith@users.noreply.github.com>
2025-04-22 17:00:26 +0100

    PAY-7766 removal of the azure.extension plpgsql (#627)
    Co-authored-by: chrisworthcgi <138458129+chrisworthcgi@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -110,7 +110,7 @@ module "fees-register-database-v15" {
   pgsql_server_configuration = [
     {
       name  = "azure.extensions"
-      value = "plpgsql,pg_stat_statements,pg_buffercache"
+      value = "pg_stat_statements,pg_buffercache"
     }
   ]
   pgsql_sku            = var.flexible_sku_name
