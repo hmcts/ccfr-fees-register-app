2f4437ab7fea995a6c2a94010887cd3956fb5453
Sammi Chong <133241942+SammiChong@users.noreply.github.com>
2026-08-07 14:11:34 +0100

    Update to Infrastructure@2.4.3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -91,7 +91,7 @@ module "fees-register-database-v15" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
   product              = var.product
   component            = var.component
   business_area        = "cft"
