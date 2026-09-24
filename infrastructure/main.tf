4125670cb4eabb9e1ced79bd1163e2a97fb4219c
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-09-24 11:56:52 +0100

    CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -121,6 +121,8 @@ module "fees-register-database-v15" {
   action_group_name          = join("-", [var.db_monitor_action_group_name, local.db_server_name, var.env])
   email_address_key          = var.db_alert_email_address_key
   email_address_key_vault_id = data.azurerm_key_vault.freg_key_vault.id
+  # Add service criticality param
+  service_criticality = var.service_criticality
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
